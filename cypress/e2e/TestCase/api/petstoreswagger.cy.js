c7bbef6987c2267501556b764f2687aa7eeea8a4
joe-b-reaker <josericardo.c.ayala@gmail.com>
2025-07-25 13:35:13 +0800

    added new test cases to api testing

--- cypress/e2e/TestCase/api/petstoreswagger.cy.js
+++ cypress/e2e/TestCase/api/petstoreswagger.cy.js
@@ -37,6 +37,20 @@ describe('Swagger - Petstore API Test Cases', () => {
             expect(response.body).to.have.property('id', 1);
      });
   });
+
+    it('Cannot retrieve user data using GET request',() =>{
+    cy.request({
+                    method: 'GET',
+            url: 'https://petstore.swagger.io/v2/user/jayala404',
+            failOnStatusCode: false
+        }).then((response) => {
+            cy.log(response);
+            expect(response.status).to.eq(404);
+            expect(response.headers['content-type']).to.include('application/json');
+            expect(response.body).to.have.property('type', 'error');
+            expect(response.body).to.have.property('code', 1);
+     });
+  });
   it('Update user data using PUT request',()=>{
     cy.request({
                     method: 'PUT',
@@ -62,6 +76,58 @@ describe('Swagger - Petstore API Test Cases', () => {
             expect(response.body).to.have.property('code', 200);
      });
   });
+
+    it.only('Update user non-existing user data using PUT request',()=>{
+    cy.request({
+                    method: 'PUT',
+            url: 'https://petstore.swagger.io/v2/user/&&jayala0990u404',
+            body : {
+                 id: '312321505',
+                username: 'jewqewqayala',
+                firstName: 'eeeJoseUpdated',
+                lastName: 'eeeAyala',
+                email: 'eee@verete-email.com',
+                password: 'password',
+                phone: '11111111111',
+                userStatus: 1
+            },
+      headers: {
+                'Content-type': 'application/json; charset=UTF-8'
+            },
+            failOnStatusCode: false
+        }).then((response) => {
+            cy.log(response);
+            expect(response.status).to.eq(200);
+            expect(response.headers['content-type']).to.include('application/json');
+            expect(response.body).to.have.property('code', 200);
+     });
+  });
+
+
+  it('Create a duplicate user using POST request', () => {
+    //user Create User
+    cy.request({
+      method: 'POST',
+      url: 'https://petstore.swagger.io/v2/user', 
+      body : {
+                id: 2,
+                username: 'jayaladuplicate',
+                firstName: 'Jose',
+                lastName: 'Ayala',
+                email: 'jayala@verete-email.com',
+                password: 'password',
+                phone: '11111111111',
+                userStatus: 1
+            },
+      headers: {
+                'Content-type': 'application/json; charset=UTF-8'
+            }
+      }).then((response) => {
+            expect(response.status).to.be.oneOf([200,404])
+            expect(response.body).to.have.property('message');
+            expect(response.body.message).to.be.a('string');
+        });
+    });
 
     it('Delete a user by using DELETE request',()=>{
     cy.request({
@@ -147,33 +213,57 @@ describe('Swagger - Petstore API Test Cases', () => {
                 'Content-type': 'application/json; charset=UTF-8'
             }
       }).then((response) => {
-            expect(response.status).to.be.oneOf([200,404]);
+            expect(response.status).to.be.to.eq(200);
             expect(response.body).to.have.property('message');
             expect(response.body).to.have.property('message','ok');
         });
     });
-  it('Create a duplicate user using POST request', () => {
-    //user Create User
+    
+  it('User can login using GET request',() =>{
     cy.request({
-      method: 'POST',
-      url: 'https://petstore.swagger.io/v2/user', 
-      body : {
-                id: 2,
-                username: 'jayaladuplicate',
-                firstName: 'Jose',
-                lastName: 'Ayala',
-                email: 'jayala@verete-email.com',
-                password: 'password',
-                phone: '11111111111',
-                userStatus: 1
+                method: 'GET',
+            url: 'https://petstore.swagger.io/v2/user/login?username=jayala&password=password',
+            failOnStatusCode: false
+        }).then((response) => {
+            cy.log(response);
+            expect(response.status).to.eq(200);
+            expect(response.headers['content-type']).to.include('application/json');
+            expect(response.body).to.have.property('code', 200);
+            expect(response.body.message).to.include('logged in user session');
+     });
+  });
+ 
+    it('User cannot login using GET request without a password',() =>{
+    cy.request({
+                method: 'GET',
+            url: 'https://petstore.swagger.io/v2/user/login?username=jayala',
+            failOnStatusCode: false
+        }).then((response) => {
+            cy.log(response);
+            expect(response.status).to.eq(200);
+            expect(response.headers['content-type']).to.include('application/json');
+            expect(response.body).to.have.property('code', 200);
+            expect(response.body.message).to.include('logged in user session');
+     });
+  });
+
+    it('User can logout',() =>{
+    cy.request({
+                method: 'GET',
+            url: 'https://petstore.swagger.io/v2/user/login?username=jayala',
+            headers: {
+                'accept': 'application/json'
             },
-      headers: {
-                'Content-type': 'application/json; charset=UTF-8'
-            }
-      }).then((response) => {
-            expect(response.status).to.be.oneOf([200,404]);
-            expect(response.body).to.have.property('message');
-            expect(response.body.message).to.be.a('string');
-        });
-    });
+            failOnStatusCode: false
+        }).then((response) => {
+            cy.log(response);
+            expect(response.status).to.eq(200);
+            expect(response.headers['content-type']).to.include('application/json');
+            expect(response.body).to.have.property('code', 200);
+            expect(response.body.message).to.include('logged in user session');
+     });
+  });  
+
+
+
 });
